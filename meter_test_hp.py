81751ca57782d3ea12b3e8248ad0cf089f1cf5f4
Jamie Bird <jamie06bird@gmail.com>
2015-01-08 18:30:53 +0000

    Updated meter test to limit all ports on switch.

--- meter_test_hp.py
+++ meter_test_hp.py
@@ -14,7 +14,9 @@ class SimpleSwitch13(app_manager.RyuApp):
     def __init__(self, *args, **kwargs):
         super(SimpleSwitch13, self).__init__(*args, **kwargs)
         self.mac_to_port = {}
+	self.port_to_meter = {}
 
+    #Called when connected to new switch
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
         datapath = ev.msg.datapath
@@ -29,18 +31,9 @@ class SimpleSwitch13(app_manager.RyuApp):
         self.add_flow(datapath, 1, match, actions, meter=None, timeout=0)
 
         print datapath.id
-        #Add flow that installs meter
-        bands = []
-        dropband = parser.OFPMeterBandDrop(rate=200, burst_size=0)
-        bands.append(dropband)
-        request = parser.OFPMeterMod(datapath=datapath,
-                                     command=ofproto.OFPMC_ADD,
-                                     flags=ofproto.OFPMF_KBPS,#ofproto.OFPMF_PKTPS
-                                     meter_id=1,
-                                     bands=bands)
-        print("Creating new meter")
-        datapath.send_msg(request);
-        self.send_meter_features_stats_request(datapath)
+    
+        #requesting meter features
+	self.send_meter_features_stats_request(datapath)
 
 
         #print out meter stats
@@ -50,33 +43,31 @@ class SimpleSwitch13(app_manager.RyuApp):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
         #Meter instruction here
-        #
-        if meter:
-            inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,actions),parser.OFPInstructionMeter(1)]
+	print "The meter is :",meter
+        if meter != None:
+	    print "Sending flow mod with meter instruction, meter :", meter
+            inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,actions),parser.OFPInstructionMeter(meter)]
         else:
+	    print "Not sending instruction"
             inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,actions)]
         if buffer_id:
-            mod = parser.OFPFlowMod(datapath=datapath, buffer_id=buffer_id,
-                                    priority=priority, match=match,
-                                    instructions=inst, hard_timeout=timeout, idle_timeout=timeout)
+            mod = parser.OFPFlowMod(datapath=datapath, buffer_id=buffer_id,priority=priority, match=match,instructions=inst, hard_timeout=timeout, idle_timeout=timeout, table_id=100)
         else:
-            mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
-                                    match=match, instructions=inst, hard_timeout=timeout, idle_timeout=timeout)
+            mod = parser.OFPFlowMod(datapath=datapath, priority=priority,match=match, instructions=inst, hard_timeout=timeout, idle_timeout=timeout, table_id=100)
         datapath.send_msg(mod)
 
     #gets the meter stats from switch periodically
-    dd = None
     def printit(self,dd):
-        threading.Timer(1.0, self.printit(dd)).start()
-        print("requesting meter stats")
+        threading.Timer(5.0, self.printit,[dd]).start()
         self.send_meter_stats_request(dd)
 
+    
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def _packet_in_handler(self, ev):
         # If you hit this you might want to increase
         # the "miss_send_length" of your switch
         if ev.msg.msg_len < ev.msg.total_len:
-            self.logger.debug("packet truncated: only %s of %s bytes",
+            self.logger.info("packet truncated: only %s of %s bytes",
                               ev.msg.msg_len, ev.msg.total_len)
 
         msg = ev.msg
@@ -87,20 +78,12 @@ class SimpleSwitch13(app_manager.RyuApp):
         in_port = msg.match['in_port']
 
 
-
-
         pkt = packet.Packet(msg.data)
         eth = None
         eth = pkt.get_protocols(ethernet.ethernet)[0]
 
-        #if eth = None
-        #    //its our packet
-        #    //do calculations
-
-
         dst = eth.dst
         src = eth.src
-
 
         dpid = datapath.id
         self.mac_to_port.setdefault(dpid, {})
@@ -117,23 +100,55 @@ class SimpleSwitch13(app_manager.RyuApp):
 
         actions = [parser.OFPActionOutput(out_port)]
 
+
+
+
+	#Create new meters
+	#Check for flood, dont want to add meter for flood
+	if out_port != ofproto.OFPP_FLOOD:	
+             print "NOT A FLOOD PACKET"
+	     if out_port in self.port_to_meter:
+                 #if the meter already exists set instruction to use	
+                 print "Meter already exists for this port"
+	     else:
+	         #This controller not added meter before, need to create one for this port
+	         print "NEW METER CREATED FOR :", out_port 
+	         bands=[]
+		 #set starting bit rate of meter
+                 dropband = parser.OFPMeterBandDrop(rate=100000, burst_size=0)
+                 bands.append(dropband)
+	         #Delete meter first, it might already exist
+	         request = parser.OFPMeterMod(datapath=datapath,command=ofproto.OFPMC_DELETE,flags=ofproto.OFPMF_KBPS,meter_id=out_port,bands=bands)
+	         datapath.send_msg(request)
+                 request = parser.OFPMeterMod(datapath=datapath,command=ofproto.OFPMC_ADD, flags=ofproto.OFPMF_KBPS,meter_id=out_port,bands=bands)
+	         datapath.send_msg(request)	    
+	         self.port_to_meter[out_port]=out_port
+
+
+
+
         # install a flow to avoid packet_in next time
         if out_port != ofproto.OFPP_FLOOD:
             match = parser.OFPMatch(in_port=in_port, eth_dst=dst)
             # verify if we have a valid buffer_id, if yes avoid to send both
             # flow_mod & packet_out
             if msg.buffer_id != ofproto.OFP_NO_BUFFER:
-                self.add_flow(datapath, 2, match, actions, msg.buffer_id, meter=True)
+		print "Valid buffer"
+                self.add_flow(datapath, 2, match, actions, msg.buffer_id, meter=out_port, timeout=60)
                 return
             else:
-                self.add_flow(datapath, 2, match, actions, meter=True)
-        data = None
+                self.add_flow(datapath, 2, match, actions, meter=out_port, timeout=60)
+        
+	data = None
         if msg.buffer_id == ofproto.OFP_NO_BUFFER:
             data = msg.data
 
         out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
                                   in_port=in_port, actions=actions, data=data)
         datapath.send_msg(out)
+
+
+
 
     #Ask switch about its meter features
     def send_meter_features_stats_request(self, datapath):
